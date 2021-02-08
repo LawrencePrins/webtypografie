8cb027857cbf44308fe22d5b90af3265a3c70690
LawrencePrins <LawrencePrins@hotmail.com>
2021-02-08 17:09:01 +0100

    Eerste test
    In css heb ik de eerste aantal belangrijke elementen geselecteerd om te zien of dit werkt. Daarnaast heb ik in javascript ieder geluid voorzien van een comment.

--- sounds.js
+++ sounds.js
@@ -1,15 +1,23 @@
 // Hier kan je tijden toevoegen in secondes
 // Op die mommenten komt er een class op de body.
 sounds = [
+// sound 0 | begin van video 
 	0,
+// sound 1 | intro alarm, enkel geluid
 	9,
+// sound 2 | klinkt als korte politie sirene
 	12,
+// sound 3,4 | alarm, 2 geluiden, main alarm
 	15,
 	16.5,
+// sound 5,6 | alarm, 2 geluiden, main alarm, iets zachter
 	24,
 	25.8,
+// sound 7,8 | alarm, 2 geluiden, main alarm
 	31,
 	32,
+// sound 9 | naar hoog piep geluid (spannend opbouw)
 	34,
+// sound 10 | background music (spannend muziek)
 	86
 ];
