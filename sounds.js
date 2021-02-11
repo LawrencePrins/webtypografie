da61ad961cc18badde22746be1a51f1d15688557
LawrencePrins <LawrencePrins@hotmail.com>
2021-02-11 23:02:11 +0100

    .voice1 styling & background
    .voice1 gestyled. In het .js bestand komt er bij iedere uitspraak van .voice1 een nieuwe div te voorschijn. Deze div is een audio visualizer (gemaakt in css) zodat het lijkt alsof .voice1 uitgeproken wordt uit een intercom.
    
    Daarnaast is de background van de body toegepast.

--- sounds.js
+++ sounds.js
@@ -3,21 +3,65 @@
 sounds = [
 // sound 0 | begin van video 
 	0,
-// sound 1 | intro alarm, enkel geluid
-	9,
-// sound 2 | klinkt als korte politie sirene
-	12,
-// sound 3,4 | alarm, 2 geluiden, main alarm
-	15,
-	16.5,
-// sound 5,6 | alarm, 2 geluiden, main alarm, iets zachter
-	24,
-	25.8,
-// sound 7,8 | alarm, 2 geluiden, main alarm
-	31,
-	32,
-// sound 9 | naar hoog piep geluid (spannend opbouw)
-	34,
-// sound 10 | background music (spannend muziek)
-	86
+// sound 1 | speech "Officer K D 6 - 3 . 7. Let’s begin. Ready?"
+    4,
+// sound 2 | alarm
+    9,
+// sound 3 | speech "Recite your baseline"
+    11,
+// sound 4 | police alarm
+    12,
+// sound 5 | main alarm
+    15,
+// sound 6 | main alarm
+    16,
+// sound 7 | main alarm
+    17,
+// sound 8 | speech "Cells"
+    32.8,
+// sound 9 | speech "Have you ever been in institution?"
+    34,
+//sound 10 | speech "Cells"
+    36.3,
+//sound 11 | speech "Do they keep you in a cell?"
+    37.3,
+//sound 12 | speech "Cells"
+    38.9,
+//sound 13 | speech "When you're not performing your duties, do they keep you in a little box?"
+    40,
+//sound 14 | speech "Cells"
+    43,
+//sound 15 | speech "Interlinked"
+    44,
+//sound 16 | speech "What's it like to hold the hand of someone you love?"
+    45.8,
+//sound 17 | speech "Interlinked"
+    47.8,
+//sound 18 | speech "Did they teach you how to feel finger to finger?"
+    49.2,
+//sound 19 | speech "Interlinked"
+    51,
+//sound 20 | speech "Do you long for having your heart interlinked?"
+    52.2,
+//sound 21 | speech "Interlinked"
+    54,
+//sound 22 | speech "Do you dream about being interlinked?"
+    55.5,
+//sound 23 | speech "What's it like to hold your child in your arms?"
+    58,
+//sound 24 | speech "Interlinked"
+    60.,
+//sound 25 | speech "Do you feel that there is a part of you missing?"
+    61,
+//sound 26 | speech "Interlinked"
+    62.7,
+//sound 27 | speech "Within cells interlinked"
+    64,
+//sound 28 | speech "Why dont you say that 3 times: Within cells interlinked"
+    66.7,
+//sound 29 | music stops.
+    75,
+//sound 30 | speech "we're done"
+    77,
+
 ];
